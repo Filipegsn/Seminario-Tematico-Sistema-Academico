a389ac9d3310f3128d165169df62a248a0423f04
Filipegsn <42592776+Filipegsn@users.noreply.github.com>
2018-10-18 21:26:27 -0300

    Login em html

---
+++ tela_login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TELA</title>
+	<link rel="stylesheet" type="text/css" href="tela_login.css">
+</head>
+<body>
+	<div class="box">
+		<ul>Login</ul>
+		<ul>Senha</ul>
+		<ul class="botao">Entrar</ul>
+	</div>	
+</body>
+</html>
