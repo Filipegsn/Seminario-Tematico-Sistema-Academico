f681f967adc3b8f4a1030e8a408deb1c1aa6f2a1
Filipegsn <42592776+Filipegsn@users.noreply.github.com>
2018-10-25 16:45:40 -0300

    Criação e organização de novas DIVS

--- tela_login.php
+++ tela_login.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>TELA</title>
+	<title>TELA DE LOGIN</title>
 	<link rel="stylesheet" type="text/css" href="tela_login.css">
 </head>
 <body>
-	<div class="box">
-		<ul>Login</ul>
-		<ul>Senha</ul>
-		<ul class="botao">Entrar</ul>
-	</div>	
+	<div class="img_login">
+		<div class="box">
+			<div class="logo">
+				<img src="logo.png">
+			</div>
+			<h1>Sistema Acadêmico</h1>
+		<form method="POST" action="login_acesso.php">
+			<ul>Matricula</ul>
+			<input autofocus type="txt" name="login" placeholder="Insira sua Matricula" required/>
+			<ul>Senha</ul>
+			<input type="password" name="senha" placeholder="Insira sua Senha" required/>
+			<!--<input type="submit" value="Entrar" name="btn_entrar">-->
+		</form>
+			<ul class="botao1" input type="submit" value="Entrar" name="btn_entrar">Confirmar</ul>
+			<!--<ul class="botao2">Voltar</ul>-->
+		</div>
+	</div>
 </body>
 </html>
