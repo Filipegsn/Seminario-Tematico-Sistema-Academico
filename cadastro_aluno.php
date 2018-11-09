f9bde1d8acd251f9ad9671f55e68761b3ae66f75
Filipegsn <42592776+Filipegsn@users.noreply.github.com>
2018-11-09 17:14:19 -0200

    Update cadastro_aluno.php

--- cadastro_aluno.php
+++ cadastro_aluno.php
@@ -13,9 +13,13 @@
 			</div>
 			<h1>Sistema Acadêmico</h1>
 		<form method="POST" action="login_acesso.php">
-			<ul>Matricula</ul>
-				<input autofocus type="txt" name="login" placeholder="Insira a Matricula" required/>
-			<ul>Curso
+			<ul class="nome">Nome:</ul>
+				<input autofocus type="txt" name="nome" placeholder="Nome Completo" required/>
+			<ul class="matricula">Matricula:</ul>
+				<input autofocus type="txt" name="matricula" placeholder="Insira a Matricula" required/>
+			<ul class="senha">Senha:</ul>
+				<input autofocus type="txt" name="senha" placeholder="Defina uma Senha" required/>
+			<ul class="curso">Curso
 				<!--<input type="txt" name="curso" placeholder="Escoha o Curso" required/>-->
 				<select name="nome_curso">
 					<option value="">Administração</option>
@@ -23,7 +27,7 @@
 					<option>Direito</option>
 				</select>
 			</ul>
-			<ul>Turno
+			<ul class="turno1">Turno
 				<!--<input type="txt" name="turno" placeholder="Escoha o Turno" required/>-->
 				<select name="turno">
 					<option>Matutino</option>
@@ -31,8 +35,41 @@
 					<option>Noturno</option>
 				</select>
 			</ul>
+			<ul class="func">Função
+				<!--<input type="txt" name="turno" placeholder="Escoha o Turno" required/>-->
+				<select name="funcao">
+					<option>Professor</option>
+					<option>Aluno</option>
+				</select>
+			</ul>
 
 			<!--<input type="submit" value="Entrar" name="btn_entrar">-->
+
+		<?php
+  require 'conexao_dbacademic.php';
+  require 'classe_user.php';
+
+  $user = new User($pdo);
+
+    
+  if (isset($_POST['nome']) && !empty($_POST['nome'])) {
+
+    $user->setNome($_POST['nome']);
+    $user->setMatricula($_POST['matricula']);
+    $user->setSenha($_POST['senha']);
+
+    /*
+    $email = addslashes($_POST['email']);
+    $senha =$_POST['senha'];*/
+    
+    $user->cadastrar($user);
+    //$usuario->cadastrar($email,$senha);
+    header("Location: index.php");
+    exit;
+    
+  }
+  
+?>
 		</form>
 			<ul class="botao1" input type="submit" value="Entrar" name="btn_entrar">Confirmar</ul>
 			<!--<ul class="botao2">Voltar</ul>-->
