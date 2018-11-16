fb02b48da5513052aeb163b0a12ebd1e2de203e2
Rodolfo Azevedo <rodolfogmazevedo@gmail.com>
2018-11-15 21:50:24 -0300

    áreas de trabalho prof e aluno' ;

---
+++ portal_aluno.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Portal do Aluno</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<div class="background">
+		<div class="topo">
+			<div class="subtopo">
+				<dir class="info">
+					<h2>
+						aluno fulano
+					</h2>
+				</dir>
+				<div class="logo">
+					<h2>Sistema Acadêmico - Área do Aluno</h2>
+				</div>
+			</div>
+		</div>
+
+		<div class="menu">
+			<h1>Menu</h1>
+				<ul>
+					<li>Boletim</li>
+					<li>Faltas</li>
+					<li>Imprimir Boleto</li>
+					<li>Sair</li>
+				</ul>
+		</div>
+	</div>
+</body>
+</html>
---
+++ portal_prof.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Portal do Professor</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<div class="background">
+		<div class="topo">
+			<div class="subtopo">
+				<dir class="info">
+					<h2>
+						Informações do Professor
+					</h2>
+				</dir>
+				<div class="logo">
+					<h2> Área do Professor</h2>
+				</div>
+			</div>
+		</div>
+
+		<div class="menu">
+			<h1>Menu</h1>
+				<ul>
+					<li>Lançar Notas</li>
+					<li>Lançar Faltas</li>
+					<li>Sair</li>
+				</ul>
+		</div>
+	</div>
+</body>
+</html>
